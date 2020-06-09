a00ff02bddb814a635865fb1a851ef18fbd69b62
Tominous <18189490+Tominous@users.noreply.github.com>
2020-06-09 16:05:07 +0100

    Update commands.js

--- dashboard/routes/commands.js
+++ dashboard/routes/commands.js
@@ -14,7 +14,7 @@ router.get("/", CheckAuth, async (req, res) => {
             xtal: req.xtal,
             size: req.xtal.commands.size,
             cmds: req.xtal.commands.map(x => `<font size="14"><b>x?${x.help.name}</font></b><br><font size="4">Aliases: ${x.help.aliases.length > 0 ? x.help.aliases.map(z => `x?${z}`).join(", ") : "None"}<br>Desc: ${x.conf.description}<br>Category: ${x.conf.category}<br>Usage: x?${x.conf.usage}</font>`).join("<hr>")
-            //cmds: req.xtal.commands.map(x => `<b>${x.help.name}</b><br>Aliases: ${x.help.aliases.join(", ")}<br>Desc: ${x.conf.description}<br>Category: ${x.conf.category}<br>Usage: ${x.conf.usage}<br>Examples: ${x.conf.examples.length > 0 ? x.conf.examples.join(", ") : "None"}<br>Permissions: ${x.conf.memberPermissions.length > 0 ? x.conf.memberPermissions.join(", ") : "None"}<br>Guild Only: ${x.conf.guildOnly ? "Yes" : "No"}`).join("<hr>")
+            cmds: req.xtal.commands.map(x => `<b>${x.help.name}</b><br>Aliases: ${x.help.aliases.join(", ")}<br>Desc: ${x.conf.description}<br>Category: ${x.conf.category}<br>Usage: ${x.conf.usage}<br>Examples: ${x.conf.examples.length > 0 ? x.conf.examples.join(", ") : "None"}<br>Permissions: ${x.conf.memberPermissions.length > 0 ? x.conf.memberPermissions.join(", ") : "None"}<br>Guild Only: ${x.conf.guildOnly ? "Yes" : "No"}`).join("<hr>")
         });
     }
 });
