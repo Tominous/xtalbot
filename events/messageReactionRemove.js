11ba6993fbd3260252d3a6fb07575680c10d4b9d
Tominous <18189490+Tominous@users.noreply.github.com>
2020-06-09 16:08:27 +0100

    Update messageReactionRemove.js

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -19,7 +19,7 @@ module.exports = async (xtal, reaction, user) => {
   .setTimestamp()
   .setDescription(`[Jump to Message](${message.url})`)
   .addField('Author', `${message.author.tag} | ${message.author.id}`)
-  .addField(`Reaction Info`, `${reaction.emoji} | ${reaction.emoji.name || 'No Name'} | ${reaction.emoji.id || 'No ID'}`)
+  .addField(`Reaction Information`, `${reaction.emoji} | ${reaction.emoji.name || 'No Name'} | ${reaction.emoji.id || 'No ID'}`)
   .addField('Message Info', `${message.channel} | ${message.channel.name} | ${message.channel.id}`)
   .setColor(colors.purple)
   .setFooter(xtal.user.username, xtal.user.avatarURL);
